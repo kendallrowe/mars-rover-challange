4090f42200785b1d2c2bae43451a93572000057c
kendallrowe <kendall.rowe312@gmail.com>
2021-08-04 21:26:22 -0400

    Set up mission initialization tests

---
+++ Mission.test.js
@@ -0,0 +1,20 @@
+const Mission = require("./Mission");
+const Plateau = require("./Plateau");
+const Rover = require("./Rover");
+
+describe("Mission initialization tests", () => {
+    
+    const instructions = `Plateau:5 5
+    Rover1 Landing:1 2 N
+    Rover1 Instructions:LMLMLMLMM
+    Rover2 Landing:3 3 E
+    Rover2 Instructions:MMRMMRMRRM`;
+
+    const plateau = new Plateau(5, 5);
+    const testMission = new Mission(instructions, plateau);
+
+    test("if Mission can be initialized with instructions and plateau", () => {
+        expect(testMission.instructions).toBe(instructions);
+        expect(testMission.board.width).toBe(5);
+    });
+});
