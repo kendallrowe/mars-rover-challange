a8d88e6884e5931eec11c93eadb4b9e40e8615c5
Kendall Rowe <kendall.rowe312@gmail.com>
2021-08-05 15:17:10 -0400

    Added read line unit tests for mission

--- Mission.test.js
+++ Mission.test.js
@@ -1,5 +1,6 @@
 const Mission = require("./Mission");
 const Plateau = require("./Plateau");
+const Rover = require("./Rover");
 
 describe("Mission initialization tests", () => {
     
@@ -34,41 +35,55 @@ describe("Read line method tests", () => {
         const instruction = "Rover1 Landing:1 2 N"
 
         testMission.readLine(instruction);
-        expect(testMission.plateau.width).toBe(5);
+        expect(testMission.plateau.activeRover.id).toBe(1)
+        expect(testMission.plateau.rovers.length).toBe(0)
     });
 
     test("if read line of movement instruction moves a rover", () => {
         // Rover Movement execution
-        const instruction = "Rover1 Instructions:LMLMLMLMM"
+        const instruction = "Rover1 Instructions:LM"
 
         testMission.readLine(instruction);
-        expect(testMission.plateau.width).toBe(5);
+        expect(testMission.plateau.activeRover.direction).toBe("W");
+        expect(testMission.plateau.activeRover.x).toBe(0);
+        expect(testMission.plateau.activeRover.x).toBe(2);
     });
 
     test("if read line of movement instruction for a destroyed rover triggers no event", () => {
+        testMission.plateau.addRover(new Rover(5, 32, 32, "N"));
         // Rover Movement execution
-        const instruction = "Rover1 Instructions:LMLMLMLMM"
+        const instruction = "Rover5 Instructions:LM"
 
         testMission.readLine(instruction);
-        expect(testMission.plateau.width).toBe(5);
+        expect(testMission.plateau.activeRover).toBeNull();
+        expect(testMission.plateau.rovers.length).toBe(2);
+        expect(testMission.plateau.rovers[1].status).toBe("Destroyed");
     });
 });
 
 describe("Multiple rover method tests", () => {
-    test("if read line of plateau correctly instantiates a plateau on to the mission", () => {
+
+    const testMission = new Mission(instructions);
+    testMission.plateau = new Plateau(5, 5);
+    testMission.plateau.addRover(new Rover(1, 0, 0, "N"));
+
+    test("if read line adding a second rover correctly adds and activates new rover", () => {
         // Rover Movement execution
-        const instruction = "Rover1 Instructions:LMLMLMLMM"
+        const instruction = "Rover2 Landing:3 3 E"
 
         testMission.readLine(instruction);
-        expect(testMission.plateau.width).toBe(5);
+        expect(testMission.plateau.activeRover.id).toBe(2)
+        expect(testMission.plateau.rovers.length).toBe(1)
     });
 
-    test("if read line of plateau correctly instantiates a plateau on to the mission", () => {
+    test("if read line of moving second rover moves the active rover", () => {
         // Rover Movement execution
-        const instruction = "Rover1 Instructions:LMLMLMLMM"
+        const instruction = "Rover2 Instructions:RMM"
 
         testMission.readLine(instruction);
-        expect(testMission.plateau.width).toBe(5);
+        expect(testMission.plateau.activeRover.direction).toBe("S");
+        expect(testMission.plateau.activeRover.x).toBe(3);
+        expect(testMission.plateau.activeRover.x).toBe(1);
     });
 });
 // Output messages
